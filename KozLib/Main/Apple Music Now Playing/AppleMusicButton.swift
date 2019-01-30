b4bf9602c676a383cc70524e1fd4907214a04152
Kelvin Kosbab <kelvin@kosbab.com>
2019-01-30 02:05:16 -0800

    Improved apple music button animations.

--- KozLib/Main/Apple Music Now Playing/AppleMusicButton.swift
+++ KozLib/Main/Apple Music Now Playing/AppleMusicButton.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class AppleMusicButton : UIButton {
   
-  private let buttonLayer = CALayer()
-  
-  private lazy var touchCircle: CAShapeLayer = {
-    let layer = CAShapeLayer()
-    layer.path = UIBezierPath(ovalIn: CGRect(center: CGPoint(x: self.layer.bounds.midX, y: self.layer.bounds.midY), size: self.layer.bounds.size * 2.1)).cgPath
-    layer.fillColor = UIColor.gray.cgColor
-    layer.opacity = 0.0
-    return layer
-  }()
+  // MARK: - Init
   
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -37,61 +29,132 @@ class AppleMusicButton : UIButton {
     self.backgroundColor = .clear
     
     self.buttonLayer.frame = self.bounds
-    self.buttonLayer.addSublayer(self.touchCircle)
+    self.buttonLayer.addSublayer(self.circleLayer)
     
     self.layer.insertSublayer(self.buttonLayer, at: 0)
   }
   
-  // MARK: - Layer Animations
+  // MARK: - Configurable Properties
+  
+  var onPressTransform: CGAffineTransform = CGAffineTransform(scaleX: 0.85, y: 0.85)
+  
+  var onPressBoundsMultiplier: CGFloat = 2 {
+    didSet {
+      self.circleLayer.path = self.circleLayerPath
+    }
+  }
+  
+  var circleLayerColor: UIColor? = .gray {
+    didSet {
+      self.circleLayer.fillColor = self.circleLayerColor?.cgColor
+    }
+  }
+  
+  // MARK: - Layers
+  
+  private let buttonLayer = CALayer()
+  
+  private lazy var circleLayer: CAShapeLayer = {
+    let layer = CAShapeLayer()
+    layer.path = self.circleLayerPath
+    layer.fillColor = self.circleLayerColor?.cgColor
+    layer.opacity = 0.0
+    return layer
+  }()
+  
+  private var circleLayerPath: CGPath {
+    let center = CGPoint(x: self.layer.bounds.midX, y: self.layer.bounds.midY)
+    let minDimension = min(self.layer.bounds.width, self.layer.bounds.height) * 2
+    let size = CGSize(width: minDimension, height: minDimension)
+    let rect = CGRect(origin: center.applying(CGAffineTransform(translationX: size.width / -2, y: size.height / -2)), size: size)
+    return UIBezierPath(ovalIn: rect).cgPath
+  }
+  
+  // MARK: - AnimationState
   
   private enum AnimationState {
-    case `default`
+    case normal
     case pressed
-  }
-  
-  private var animationState: AnimationState = .`default`
-  
-  private let touchDownOpacity: Float = 0.2
-  private let touchUpOpacity: Float = 0
-  private let touchDownDuration: CFTimeInterval = 0.1
-  private let touchUpDuration: CFTimeInterval = 0.5
-  
-  private var timingFunction: CAMediaTimingFunction {
-    return CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
-  }
-  
-  // MARK: - Touch Down
-  
-  func animateTouchDown() {
-    self.animateOpacityTouchDown()
-  }
-  
-  private func animateOpacityTouchDown() {
-    let animation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
-    animation.fromValue = self.touchUpOpacity
-    animation.toValue = self.touchDownOpacity
-    animation.duration = self.touchDownDuration
-    animation.timingFunction = self.timingFunction
     
-    self.touchCircle.opacity = self.touchDownOpacity
-    self.touchCircle.add(animation, forKey: #keyPath(CALayer.opacity))
-  }
-  
-  // MARK: - Touch Up
-  
-  func animateTouchUp() {
-    self.animateOpacityTouchUp()
-  }
-  
-  private func animateOpacityTouchUp() {
-    let animation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
-    animation.fromValue = self.touchDownOpacity
-    animation.toValue = self.touchUpOpacity
-    animation.duration = self.touchUpDuration
-    animation.timingFunction = self.timingFunction
+    var fromOpacity: Float {
+      switch self {
+      case .normal: return 0.2
+      case .pressed: return 0
+      }
+    }
     
-    self.touchCircle.opacity = self.touchUpOpacity
-    self.touchCircle.add(animation, forKey: #keyPath(CALayer.opacity))
+    var toOpacity: Float {
+      switch self {
+      case .normal: return 0
+      case .pressed: return 0.2
+      }
+    }
+    
+    var animationDuration: CFTimeInterval {
+      switch self {
+      case .normal: return 0.1
+      case .pressed: return 0.5
+      }
+    }
+    
+    var timingFunction: CAMediaTimingFunction {
+      switch self {
+      case .normal: return CAMediaTimingFunction(name: .easeIn)
+      case .pressed: return CAMediaTimingFunction(name: .easeOut)
+      }
+    }
+    
+    var animationOptions: AnimationOptions {
+      switch self {
+      case .normal: return .curveEaseIn
+      case .pressed: return .curveEaseOut
+      }
+    }
+  }
+  
+  private var animationState: AnimationState = .normal
+  
+  // MARK: - Performing animations
+  
+  private func animate(for animationState: AnimationState) {
+    CATransaction.begin()
+    CATransaction.setAnimationDuration(animationState.animationDuration)
+    CATransaction.setAnimationTimingFunction(animationState.timingFunction)
+    
+    // Circle layer opacity animation
+    self.animateCircleLayerOpacity(for: animationState, isBatchAnimation: true)
+    
+    // Size transform animation
+    self.animateScale(for: animationState)
+    
+    // Commit the animation
+    CATransaction.commit()
+  }
+  
+  private func animateCircleLayerOpacity(for animationState: AnimationState, isBatchAnimation: Bool) {
+    let animation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+    animation.fromValue = animationState.fromOpacity
+    animation.toValue = animationState.toOpacity
+    if isBatchAnimation {
+      animation.duration = animationState.animationDuration
+      animation.timingFunction = animationState.timingFunction
+    }
+    
+    self.circleLayer.opacity = animationState.toOpacity
+    self.circleLayer.add(animation, forKey: #keyPath(CALayer.opacity))
+  }
+  
+  private func animateScale(for animationState: AnimationState) {
+    UIView.animate(withDuration: animationState.animationDuration / 2, delay: 0, options: animationState.animationOptions, animations: { [weak self] in
+      if let strongSelf = self {
+        switch animationState {
+        case .normal:
+          strongSelf.transform = .identity
+        case .pressed:
+          strongSelf.transform = strongSelf.onPressTransform
+        }
+      }
+    })
   }
   
   // MARK: - Touches
@@ -101,16 +164,16 @@ class AppleMusicButton : UIButton {
     
     if self.animationState != .pressed {
       self.animationState = .pressed
-      self.animateTouchDown()
+      self.animate(for: .pressed)
     }
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
     super.touchesEnded(touches, with: event)
     
-    if self.animationState != .default {
-      self.animationState = .default
-      self.animateTouchUp()
+    if self.animationState != .normal {
+      self.animationState = .normal
+      self.animate(for: .normal)
     }
   }
 }
